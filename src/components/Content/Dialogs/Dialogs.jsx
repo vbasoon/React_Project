44bd4458adeb2fd1d0668a347dde76e3626b7df6
Басун Володимир <vbasoon@gmail.com>
2020-07-27 12:43:07 +0300

    grid-columns

--- src/components/Content/Dialogs/Dialogs.jsx
+++ src/components/Content/Dialogs/Dialogs.jsx
@@ -3,8 +3,34 @@ import style from './Dialogs.module.css'
 
 const Dialogs = (props) => {
    return (
-      <div>
-         Dialogs
+      <div className={style.dialogs}>
+         <div className={style.dialogsItem}>
+            <div className={style.dialog}>
+               Dima
+            </div>
+            <div className={style.dialog}>
+               Lena
+            </div>
+            <div className={style.dialog}>
+               Sveta
+            </div>
+            <div className={style.dialog}>
+               Roma
+            </div>
+            <div className={style.dialog}>
+               Nataha
+            </div>
+            <div className={style.dialog}>
+               Sasha
+            </div>
+         </div>
+         <div className={style.messages}>
+
+            <div className={style.message}>Hi</div>
+            <div className={style.message}>How are u doing?</div>
+            <div className={style.message}>Thanx</div>
+
+         </div>
       </div>
    )
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import style from './Sidebar.module.css'
 import './Sidebar.css'
+import { NavLink } from 'react-router-dom';
 const Sidebar = () => {
    return (
-      <aside className="sidebar">Sidebar
+      <aside className="sidebar">
          <nav>
             <div className="container">
                <ul className="nav-menu">
-                  <li className={style.item}><a href="/profile" className="active">Profile</a></li>
-                  <li className={style.item}><a href="/messages">Message</a></li>
-                  <li className={style.item}><a href="/news">News</a></li>
-                  <li className={style.item}><a href="/music" className="disabled"> Music</a></li>
+                  <li className={style.item}><NavLink to="/profile" activeClassName={style.activeLink}>Profile</NavLink></li>
+                  <li className={style.item}><NavLink to="/messages" activeClassName={style.activeLink}>Message</NavLink></li>
+                  <li className={style.item}><NavLink to="/news" activeClassName={style.activeLink}>News</NavLink></li>
+                  <li className={style.item}><NavLink to="/music" activeClassName={style.activeLink}> Music</NavLink></li>
                   <hr />
-                  <li className={style.item}><a href="/settings">Settings</a></li>
+                  <li className={style.item}><NavLink to="/settings" activeClassName={style.activeLink}>Settings</NavLink></li>
                </ul>
             </div>
          </nav >
