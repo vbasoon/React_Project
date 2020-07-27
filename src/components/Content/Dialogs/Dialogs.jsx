a29e61f832d6d23dd7ab3b2e47ab0bcd3b41648a
Басун Володимир <vbasoon@gmail.com>
2020-07-27 08:32:23 +0300

    Creating components Dialogs

---
+++ src/components/Content/Dialogs/Dialogs.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './Dialogs.module.css'
+
+const Dialogs = (props) => {
+   return (
+      <div className={style.main}>Dialogs</div>
+   )
+}
+
+export default Dialogs
