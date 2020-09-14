04f46fac67def2b6afd69f52cebbc6d9b76f8e6a
Басун Володимир <vbasoon@gmail.com>
2020-09-14 09:32:55 +0300

    refactoring store

--- src/index.js
+++ src/index.js
@@ -26,7 +26,10 @@ let rerenderEntireTree = (state) => {
 
 rerenderEntireTree(store.getState());
 
-store.subscribe(rerenderEntireTree);
+store.subscribe(() => {
+   let state = store.getState();
+   rerenderEntireTree(state);
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
