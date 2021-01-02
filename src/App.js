91b215121292b307fadc37e97e2fe1402861c080
Басун Володимир <vbasoon@gmail.com>
2021-01-02 09:57:53 +0200

    Users Page begin

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Profile from "./components/Content/Profile/Profile.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import DialogsContainer from "./components/Content/Dialogs/DialogsContainer";
 import { Route } from "react-router-dom";
+import Users from './components/Content/Dialogs/Users/Users';
 
 const App = (props) => {
   return (
@@ -22,6 +23,8 @@ const App = (props) => {
             path="/messages"
             render={() => <DialogsContainer />}
           />
+          <Route path="/users"
+            render={() => <Users />} />
           <Route path="/news" />
           <Route path="/music" />
           <Route path="/settings" />
---
+++ src/components/Content/Dialogs/Users/Users.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Users = (props) => {
+   return (
+      <div>
+         Users Page
+         
+      </div>
+   )
+}
+
+export default Users
