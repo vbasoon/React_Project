2ec1cff955e07f12df9e1e2f90b97875a47ef01e
Басун Володимир <vbasoon@gmail.com>
2021-01-15 09:04:38 +0200

    const follow unfollow

--- src/redux/usersReduser.js
+++ src/redux/usersReduser.js
@@ -1,3 +1,15 @@
+const FOLLOW = 'FOLLOW';
+const UNFOLLOW = 'UNFOLLOW';
+
+let initialState = {
+   users: [
+      { id: 1, followed: false, nickName: 'Dmitry', status: 'I am a boss', location: { city: 'Mensk', country: 'Belarus' } },
+      { id: 2, followed: true, nickName: 'Rasputin', status: 'I am a boss too', location: { city: 'Moscow', country: 'Ruzzia' } },
+      { id: 3, followed: false, nickName: 'Pavlo', status: 'I am a boss too', location: { city: 'Kyiv', country: 'Ukraine' } }
+   ]
+}
+
+
 const usersReducer = (state = initialState, action) => {
 
    switch (action.type) {
