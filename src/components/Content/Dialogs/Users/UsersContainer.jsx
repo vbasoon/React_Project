69da5a2c8a606a9cd006a16aa9d6563b4637ceee
Басун Володимир <vbasoon@gmail.com>
2021-01-15 11:36:39 +0200

    mapStateToProps

--- src/components/Content/Dialogs/Users/UsersContainer.jsx
+++ src/components/Content/Dialogs/Users/UsersContainer.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 import Users from './Users';
 import { connect } from 'react-redux';
 
-let mapStateToProps = (state) = {}
-let mapDispatchToProps = (state) = {}
+let mapStateToProps = (state) => {
+   return {
+      users: state.usersPage.users
+   }
+}
+let mapDispatchToProps = (state) => {
+   return {
+
+   }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(Users);
 
